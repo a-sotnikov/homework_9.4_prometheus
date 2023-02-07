42727a9e4d0a3325e9d54574933d5aa3bc02cb70
a-sotnikov <sotnikov.andrey07@gmail.com>
2023-02-07 22:33:20 +0300

    add terraform

---
+++ cloud-terraform/main.tf
@@ -0,0 +1,94 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+provider "yandex" {
+  zone = "ru-central1-b"
+}
+
+resource "yandex_compute_instance" "vm-1" {
+  name = "prometheus-server"
+  hostname = "prometheus-server"
+
+  resources {
+    cores  = 2
+    core_fraction = 5
+    memory = 1
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8emvfmfoaordspe1jr"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    "ssh-keys" = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+}
+
+resource "yandex_compute_instance" "vm-2" {
+  name = "prometheus-node"
+  hostname = "prometheus-node"
+
+  resources {
+    cores  = 2
+    core_fraction = 5
+    memory = 1
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8emvfmfoaordspe1jr"
+    }
+  }
+
+  network_interface {
+    subnet_id = yandex_vpc_subnet.subnet-1.id
+    nat       = true
+  }
+
+  metadata = {
+    "ssh-keys" = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+}
+
+resource "yandex_vpc_network" "network-1" {
+  name = "network1"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-b"
+  network_id     = yandex_vpc_network.network-1.id
+  v4_cidr_blocks = ["192.168.100.0/24"]
+
+}
+
+output "internal_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
+}
+
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+
+output "external_ip_address_vm_1" {
+  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+}
+
+output "external_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+}
